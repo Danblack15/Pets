d45e09db3b21ffbef4b44a185e58fa03934ee764
Даниил <66919708+Danblack15@users.noreply.github.com>
2022-03-28 22:15:53 +0500

    UI Button
    Обновлен UIButton

--- src/components/ButtonsBlock/ButtonsBlock.vue
+++ src/components/ButtonsBlock/ButtonsBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="buttons-item">
-    <p class="buttons-item-name">Desktop кнопка</p>
+    <p class="buttons-item-name">Кнопка обычная</p>
     <ButtonUI>Напиши сейчас</ButtonUI>
     <input 
         type="text" 
@@ -11,134 +11,45 @@
   </div>
 
   <div class="buttons-item">
-    <p class="buttons-item-name">CardDesktop кнопка</p>
-    <ButtonUI cardDesktop>Купить - 5000</ButtonUI>
+    <p class="buttons-item-name">Кнопка маленькая</p>
+    <ButtonUI small>Купить - 5000</ButtonUI>
     <input 
         type="text" 
         class="buttons-item-tag"
-        value="<ButtonUI cardDesktop></ButtonUI>"
+        value="<ButtonUI small></ButtonUI>"
         disabled
     />
   </div>
 
   <div class="buttons-item">
-    <p class="buttons-item-name">AuthDesktop кнопка</p>
-    <ButtonUI auth>Войти</ButtonUI>
+    <p class="buttons-item-name">Кнопка большая</p>
+    <ButtonUI large>Купить - 5000</ButtonUI>
     <input 
         type="text" 
         class="buttons-item-tag"
-        value="<ButtonUI auth></ButtonUI>"
+        value="<ButtonUI large></ButtonUI>"
         disabled
     />
   </div>
 
   <div class="buttons-item">
-    <p class="buttons-item-name">AuthDesktop Disabled кнопка</p>
-    <ButtonUI auth class="disabled">Войти</ButtonUI>
-    <input 
-        type="text" 
-        class="buttons-item-tag"
-        value="<ButtonUI auth class='disabled'></ButtonUI>"
-        disabled
-    />
-  </div>
-  
-  <div class="buttons-item">
-    <p class="buttons-item-name">Laptop кнопка</p>
-    <ButtonUI laptop>Напиши сейчас</ButtonUI>
-    <input 
-        type="text" 
-        class="buttons-item-tag"
-        value="<ButtonUI laptop></ButtonUI>"
-        disabled
-    />
-  </div>
-
-  <div class="buttons-item">
-    <p class="buttons-item-name">CardLaptop кнопка</p>
-    <ButtonUI cardLaptop>Купить - 5000</ButtonUI>
-    <input 
-        type="text" 
-        class="buttons-item-tag"
-        value="<ButtonUI cardLaptop></ButtonUI>"
-        disabled
-    />
-  </div>
-
-  <div class="buttons-item">
-    <p class="buttons-item-name">AuthLaptop кнопка</p>
-    <ButtonUI auth>Войти</ButtonUI>
-    <input 
-        type="text" 
-        class="buttons-item-tag"
-        value="<ButtonUI auth></ButtonUI>"
-        disabled
-    />
-  </div>
-
-  <div class="buttons-item">
-    <p class="buttons-item-name">Tablet кнопка</p>
-    <ButtonUI tablet>Напиши сейчас</ButtonUI>
-    <input 
-        type="text" 
-        class="buttons-item-tag"
-        value="<ButtonUI tablet></ButtonUI>"
-        disabled
-    />
-  </div>
-
-  <div class="buttons-item">
-    <p class="buttons-item-name">AuthTablet кнопка</p>
-    <ButtonUI auth>Войти</ButtonUI>
-    <input 
-        type="text" 
-        class="buttons-item-tag"
-        value="<ButtonUI auth></ButtonUI>"
-        disabled
-    />
-  </div>
-
-  <div class="buttons-item">
-    <p class="buttons-item-name">Mobile кнопка</p>
-    <ButtonUI mobile>Напиши сейчас</ButtonUI>
-    <input 
-        type="text" 
-        class="buttons-item-tag"
-        value="<ButtonUI mobile></ButtonUI>"
-        disabled
-    />
-  </div>
-  
-
-  <div class="buttons-item">
-    <p class="buttons-item-name">CardMobile кнопка</p>
-    <ButtonUI cardMobile>Приютить</ButtonUI>
-    <input 
-        type="text" 
-        class="buttons-item-tag"
-        value="<ButtonUI cardMobile></ButtonUI>"
-        disabled
-    />
-  </div>
-
-  <div class="buttons-item">
-    <p class="buttons-item-name">AuthMobile wide кнопка</p>
-    <ButtonUI wideAuthMobile>Зарегистрироваться</ButtonUI>
-    <input 
-        type="text" 
-        class="buttons-item-tag"
-        value="<ButtonUI wideAuthMobile></ButtonUI>"
-        disabled
-    />
-  </div>
-
-  <div class="buttons-item">
-    <p class="buttons-item-name">Wide кнопка</p>
+    <p class="buttons-item-name">Кнопка широкая</p>
     <ButtonUI wide>Купить - 5000</ButtonUI>
     <input 
         type="text" 
         class="buttons-item-tag"
         value="<ButtonUI wide></ButtonUI>"
+        disabled
+    />
+  </div>
+  
+  <div class="buttons-item">
+    <p class="buttons-item-name">Кнопка неактивная</p>
+    <ButtonUI disabled>Купить - 5000</ButtonUI>
+    <input 
+        type="text" 
+        class="buttons-item-tag"
+        value="<ButtonUI disabled></ButtonUI>"
         disabled
     />
   </div>
--- src/components/ui/ButtonUI/ButtonUI.vue
+++ src/components/ui/ButtonUI/ButtonUI.vue
@@ -1,41 +1,6 @@
 <template>
-  <button class="button button-laptop" v-if="laptop">
-      <slot></slot>
-  </button>
-
-  <button class="button button-tablet" v-else-if="tablet">
-      <slot></slot>
-  </button>
-
-  <button class="button button-mobile" v-else-if="mobile">
-      <slot></slot>
-  </button>
-
-  <button class="button button-card-desktop" v-else-if="cardDesktop">
-      <slot></slot>
-  </button>
-
-  <button class="button button-card-laptop" v-else-if="cardLaptop">
-      <slot></slot>
-  </button>
-
-  <button class="button button-card-wide" v-else-if="wide">
-      <slot></slot>
-  </button>
-
-  <button class="button button-card-mobile" v-else-if="cardMobile">
-      <slot></slot>
-  </button>
-
-  <button class="button button-auth-desktop" v-else-if="auth">
-      <slot></slot>
-  </button>
-
-  <button class="button button-auth-mobile" v-else-if="wideAuthMobile">
-      <slot></slot>
-  </button>
-
-  <button class="button" v-else>
+  <button 
+    class="button" :class="classObject">
       <slot></slot>
   </button>
   
@@ -47,23 +12,11 @@ export default {
     name: 'ButtonUI',
 
     props: {
-      laptop: {
+      large: {
         type: Boolean,
         default: false
       },
-      tablet: {
-        type: Boolean,
-        default: false
-      },
-      mobile: {
-        type: Boolean,
-        default: false
-      },
-      cardDesktop: {
-        type: Boolean,
-        default: false
-      },
-      cardLaptop: {
+      small: {
         type: Boolean,
         default: false
       },
@@ -71,17 +24,20 @@ export default {
         type: Boolean,
         default: false
       },
-      cardMobile: {
+      disabled: {
         type: Boolean,
         default: false
-      },
-      auth: {
-        type: Boolean,
-        default: false
-      },
-      wideAuthMobile: {
-        type: Boolean,
-        default: false
+      }
+    },
+
+    computed: {
+      classObject() {
+        return {
+          smallButton: this.small,
+          largeButton: this.large,
+          wideButton: this.wide,
+          disabledButton: this.disabled
+        }
       }
     }
 }
