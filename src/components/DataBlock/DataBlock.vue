6814f569d16ae52b811a60a84e484cdbd2d54c04
Даниил <66919708+Danblack15@users.noreply.github.com>
2022-03-30 20:49:00 +0500

    DataBlock / DataUI
    Добавил UI блок Даты и Время

---
+++ src/components/DataBlock/DataBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="data-item">
+    <p class="data-item-name">Дата блок</p>
+    <DataUI />
+    <input 
+        type="text" 
+        class="data-item-tag"
+        value="<DataUI></DataUI>"
+        disabled
+    />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./DataBlock.css" scoped>
+
+</style>
---
+++ src/components/ui/DataUI/DataUI.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="data-block">
+      <div class="data-block__time">
+          <div class="data-block__time-up">
+              <div>
+                <img src="@/assets/img/clock.svg" alt="whatch">
+              </div>
+              <p>На часах у нас</p>
+          </div>
+          <h3>3:46</h3>
+      </div>
+      <div class="data-block__date">
+          <div class="data-block__date-up">
+              <div>
+                <img src="@/assets/img/calendar.svg" alt="calendary">
+              </div>
+              <p>На календаре у нас</p>
+          </div>
+          <h3>05.10.2021</h3>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: "DataUI",
+
+    props: {
+      
+    }
+}
+</script>
+
+<style src="./DataUI.css" scoped>
+
+</style>
