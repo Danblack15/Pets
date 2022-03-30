9afc34e4c874af5346a5b50d1c8deb64710d5898
Даниил <66919708+Danblack15@users.noreply.github.com>
2022-03-30 20:31:53 +0500

    ArrowsBlock / ArrowUI
    Добавил UI стрелок

---
+++ src/components/ArrowsBlock/ArrowsBlock.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="arrow-item">
+    <p class="arrow-item-name">Стрелка</p>
+    <ArrowUI />
+    <input 
+        type="text" 
+        class="arrow-item-tag"
+        value="<ArrowUI></ArrowUI>"
+        disabled
+    />
+  </div>
+
+  <div class="arrow-item">
+    <p class="arrow-item-name">Стрелка влево</p>
+    <ArrowUI left/>
+    <input 
+        type="text" 
+        class="arrow-item-tag"
+        value="<ArrowUI left></ArrowUI>"
+        disabled
+    />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./ArrowsBlock.css" scoped>
+
+</style>
---
+++ src/components/ui/ArrowUI/ArrowUI.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+        class="arrow" 
+        :class="{left: left}"
+    >
+        <img src="@/assets/img/arrow.svg" alt="arrow">
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "ArrowUI",
+
+    props: {
+        left: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style src="./ArrowUI.css" scoped>
+
+</style>
