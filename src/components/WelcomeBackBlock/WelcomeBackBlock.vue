0dec777d7c8e2a3a03ec19d880704dad5982e1d9
Даниил <66919708+Danblack15@users.noreply.github.com>
2022-03-30 20:46:23 +0500

    WelcomeBackBlock / WelcomeBackUI
    Добавил UI карточку "С возвращением"

---
+++ src/components/WelcomeBackBlock/WelcomeBackBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="welcome-back-item">
+    <p class="welcome-back-item-name">Карточка welcome back</p>
+    <WelcomeBackUI />
+    <input 
+        type="text" 
+        class="welcome-back-item-tag"
+        value="<WelcomeBackUI></WelcomeBackUI>"
+        disabled
+    />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./WelcomeBackBlock.css" scoped>
+
+</style>
---
+++ src/components/ui/WelcomeBackUI/WelcomeBackUI.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="welcome-back">
+      <div class="welcome-back__text">
+        <h2>С возвращением, <span>Владимир</span></h2>
+        <h3>Не забудь покормить своего питомца</h3>
+        <h3>Хорошего дня!</h3>
+      </div>
+      <img src="@/assets/img/welcomeImg.svg" alt="welcome_img">
+  </section>
+</template>
+
+<script>
+export default {
+    name: "WelcomeBackUI",
+
+    props: {
+        
+    }
+}
+</script>
+
+<style src="./WelcomeBackUI.css" scoped>
+
+</style>
