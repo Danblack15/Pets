3024e25f52f30d9ecce048ecda6c775847c143fa
Даниил <66919708+Danblack15@users.noreply.github.com>
2022-03-30 21:59:45 +0500

    CardPetsBlock / CardPetsUI
    Добавил карточку животного

---
+++ src/components/CardPetsBlock/CardPetsBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card-item-pets">
+    <p class="card-item-pets-name">Карточка животного</p>
+    <CardPetsUI />
+    <input 
+        type="text" 
+        class="card-item-pets-tag"
+        value="<CardPetsUI></CardPetsUI>"
+        disabled
+    />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./CardPetsBlock.css" scoped>
+
+</style>
---
+++ src/components/ui/CardPetsUI/CardPetsUI.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="card-pets">
+      <div class="card-pets__img">
+          <img src="@/assets/img/cardPets.png" alt="pet">
+      </div>
+      <div class="card-pets__info">
+          <div class="card-pets__info__name">
+              <h3 class="card-pets__info-name">Bob</h3>
+              <p class="card-pets__info-category">Котенок</p>
+          </div>
+          <div class="card-pets__info__likes">
+            <p class="card-pets__likes">326 лайка</p>
+            <img src="@/assets/img/heart.svg" alt="heart">
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: "CardPetsUI"
+}
+</script>
+
+<style src="./CardPetsUI.css" scoped>
+
+</style>
