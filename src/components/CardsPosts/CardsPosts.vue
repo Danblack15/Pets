0dedabda289ea908c99f0f04f35c40fac6f9c6e1
Даниил <66919708+Danblack15@users.noreply.github.com>
2022-03-30 20:40:23 +0500

    CardsPosts / CardPostUI
    Добавил карточки постов в UI

---
+++ src/components/CardsPosts/CardsPosts.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card-item">
+    <p class="card-item-name">Карточка поста</p>
+    <CardPostUI />
+    <input 
+        type="text" 
+        class="card-item-tag"
+        value="<CardPostUI></CardPostUI>"
+        disabled
+    />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./CardsPosts.css" scoped>
+
+</style>
---
+++ src/components/ui/CardPostUI/CardPostUI.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="card-post">
+      <div class="card-post__info">
+          <div class="card-post__info__person">
+              <AvatarUI post/>
+
+              <div class="card-post__info__person-text">
+                <h3 class="card-post-name">Ynfan</h3>
+                <h4 class="card-post-status">Пельмень с мясом.</h4>
+              </div>
+          </div>
+          <div class="card-post__info__date">
+              <p>17:20</p>
+          </div>
+      </div>
+      <div class="card-post__text">
+          <p>Мой кот постоянно ходит по краю бассейна, как будто ждет, что оттуда выпрыгнет утка.
+Я его уже не уговариваю.
+Он так сидит уже несколько лет.</p>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: "CardPostUI",
+
+    props: {
+        
+    }
+}
+</script>
+
+<style src="./CardPostUI.css" scoped>
+
+</style>
