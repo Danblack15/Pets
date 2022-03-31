3cc306343c16c3560f02417712ae6c575c9d8270
Даниил <66919708+Danblack15@users.noreply.github.com>
2022-03-31 20:35:07 +0500

    WriteNowBLock / WriteNowUI
    Добавил карточку "Напиши сейчас"

---
+++ src/components/WriteNowBlock/WriteNowBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="write-now-item">
+    <p class="write-now-item-name">Карточка Напиши сейчас</p>
+    <WriteNowUI />
+    <input 
+        type="text" 
+        class="write-now-item-tag"
+        value="<WriteNowUI></WriteNowUI>"
+        disabled
+    />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./WriteNowBlock.css" scoped>
+
+</style>
---
+++ src/components/ui/WriteNowUI/WriteNowUI.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="write-now">
+      <div class="write-now__friends">
+          <AvatarUI post class="write-now__avatar"/>
+          <AvatarUI post class="write-now__avatar"/>
+          <AvatarUI post class="write-now__avatar"/>
+          <AvatarUI post class="write-now__avatar"/>
+          <AvatarUI post class="write-now__avatar"/>
+          <div class="write-now__add">+</div>
+      </div>
+      <ButtonUI wide class="write-now-button">Напиши сейчас</ButtonUI>
+  </section>
+
+</template>
+
+<script>
+export default {
+    name: "WriteNowUI",
+
+    props: {
+       
+    }
+}
+</script>
+
+<style src="./WriteNowUI.css" scoped>
+
+</style>
