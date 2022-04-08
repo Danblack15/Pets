be8112f911b175e5d6734d230a2bc2b7787d2e08
Даниил <66919708+Danblack15@users.noreply.github.com>
2022-04-08 14:38:42 +0500

    Index.js
    Для UI компонентов

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -5,6 +5,18 @@ import CategoryUI from '@/components/ui/CategoryUI/CategoryUI'
 import NotificationUI from '@/components/ui/NotificationUI/NotificationUI'
 import LogoUI from '@/components/ui/LogoUI/LogoUI'
 import AvatarUI from '@/components/ui/AvatarUI/AvatarUI'
+import ArrowUI from '@/components/ui/ArrowUI/ArrowUI'
+import CardPostUI from '@/components/ui/CardPostUI/CardPostUI'
+import WelcomeBackUI from '@/components/ui/WelcomeBackUI/WelcomeBackUI'
+import DataUI from '@/components/ui/DataUI/DataUI'
+import CardPetsUI from '@/components/ui/CardPetsUI/CardPetsUI'
+import WriteNowUI from '@/components/ui/WriteNowUI/WriteNowUI'
+import ProgressDayUI from '@/components/ui/ProgressDayUI/ProgressDayUI'
+import ProgressGraph from '@/components/ui/ProgressGraph/ProgressGraph'
+import ProgressListUI from '@/components/ui/ProgressListUI/ProgressListUI'
+import FormUI from '@/components/ui/FormUI/FormUI'
+import StatisticUI from '@/components/ui/StatisticUI/StatisticUI'
+import StatisticLineUI from '@/components/ui/StatisticLineUI/StatisticLineUI'
 
 export default [
     ButtonUI,
@@ -13,5 +25,17 @@ export default [
     CategoryUI,
     NotificationUI,
     LogoUI,
-    AvatarUI
+    AvatarUI,
+    ArrowUI,
+    CardPostUI,
+    WelcomeBackUI,
+    DataUI,
+    CardPetsUI,
+    WriteNowUI,
+    ProgressDayUI,
+    ProgressGraph,
+    ProgressListUI,
+    FormUI,
+    StatisticUI,
+    StatisticLineUI
 ]
